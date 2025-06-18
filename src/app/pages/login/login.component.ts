d0b917bcbec731e1a6a894345ce0a4826d66f657
fefo <gabriel.yeray@gmail.com>
2025-06-19 00:13:56 +0100

    Añadido validadores a la ventana de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../shared/services/auth.service';
@@ -10,12 +10,16 @@ import { CommonModule } from '@angular/common';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public error: string='';
   public isSubmitted: boolean= false;
-  public userForm: FormGroup;
+  public userForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private _router: Router, private _authService: AuthService){
+  private _router= inject(Router);
+  private _authService= inject(AuthService);
+  private formBuilder= inject(FormBuilder);
+
+  ngOnInit(): void {
     this.userForm= this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
@@ -24,21 +28,26 @@ export class LoginComponent {
 
 
   public send(){
-    const username = this.userForm!.get('username')?.value;
-    const password = this.userForm!.get('password')?.value;
-    if(username && password){
-      this._authService.login(username, password).subscribe(
-        response => {
-          console.log('Login successful', response);
-          // Aquí puedes redirigir al usuario a la página principal o a otra página
-          this._router.navigate(['/']);
+    if(this.userForm.valid){
+      const username = this.userForm!.get('username')?.value;
+      const password = this.userForm!.get('password')?.value;
+      if(username && password){
+        this._authService.login(username, password).subscribe(
+          response => {
+            console.log('Login successful', response);
+            // Aquí puedes redirigir al usuario a la página principal o a otra página
+            this._router.navigate(['/']);
 
-        },
-        error => {
-          console.error('Login failed', error);
-          this.error = 'Fallo en el login. Por favor compruebe el usuario y contraseña.';
-        }
-      );
+          },
+          error => {
+            console.error('Login failed', error);
+            this.error = 'Fallo en el login. Por favor compruebe el usuario y contraseña.';
+          }
+        );
+      }
+    }else{
+      this.userForm.markAllAsTouched();
+      console.log('Formulario inválido');
     }
   }
 }
